30f240070617cc6f5b4f5be743f241e07f498e28
Mohammed faheem <44580643+faheem77@users.noreply.github.com>
2022-11-18 12:25:13 +0500

    ..

---
+++ Accutron.py
@@ -0,0 +1,109 @@
+import scrapy 
+from scrapy.crawler import CrawlerProcess
+
+
+class Accutron_scraper(scrapy.Spider):
+    
+    custom_settings = {
+        'DOWNLOAD_DELAY' : 0.25,
+        'RETRY_TIMES': 10,
+        # export as CSV format
+        'FEED_FORMAT' : 'csv',
+        'FEED_URI' : 'Accutron-data.csv'
+    #     "ROTATING_PROXY_LIST" : ["108.59.14.208:13040", "108.59.14.203:13040"],
+    #             "DOWNLOADER_MIDDLEWARES" : {
+    #             "rotating_proxies.middlewares.RotatingProxyMiddleware" : 610,
+    #             "rotating_proxies.middlewares.BanDetectionMiddleware" : 620}
+    }
+     
+    name= 'scraper'
+    start_urls = ['https://www.accutron-inc.com/product/']
+    
+    def parse(self, response):
+        links =response.css("a.product-list-item ::attr(href)").extract()
+        
+        
+        for link in links:
+            link = response.urljoin(link)
+            yield scrapy.Request(url=link, callback=self.parse_p_links)
+    def parse_p_links(self, response):
+        links = response.css("span.field-content>a ::attr(href)").extract()
+        
+        
+        
+        for link in links:
+            link= response.urljoin(link)
+            yield scrapy.Request(url=link, callback=self.parse_products)
+        
+        next_page =response.css('a.pager__link ::attr(href)').extract()
+        
+        for next in next_page:
+            url= response.urljoin(next)
+            yield response.follow(url=url, callback=self.parse_p_links)
+    def parse_products(self,response):
+        baseurl ='https://www.hufriedygroup.com/'
+        data_dict ={}
+        desc =response.css("div#prod-overview ::text").extract()
+        desc= ''.join(desc)
+        desc = desc.replace("  ",'')
+        desc2 = response.css("div.short-description::text").extract()
+        desc2 = ''.join(desc2)
+        desc2 = desc2.replace("  ",'')
+        desc = desc.strip() + desc2.strip()
+        images = response.css("li.easyzoom,easyzoom--overlay>a>img").css('::attr(src)').extract()
+        images = [baseurl+image for image in images]
+        vars = response.xpath("//tr[@class='variation-item']")
+        if len(vars)> 0:
+            for var in vars:
+                link= var.css("::attr(onclick)").extract()[0].split("= '")[1].replace("';",'')
+                link = response.urljoin(link)
+                yield scrapy.Request(url=link, callback=self.parse_variations)
+
+        else:
+            data_dict['Seller Platform']= 'Accutron'
+            data_dict['Seller SKU']= response.css('sku.prod-details__sku ::text').extract_first()
+            data_dict['Manufacture']='Accutron'
+            data_dict['Manufacture Code']=response.css('sku.prod-details__sku ::text').extract_first()
+            data_dict['Product Title']= response.css("span.page__title ::text").extract_first()
+            data_dict['Description']=desc
+            data_dict['Packaging']=''
+            data_dict['Qty']= ''
+            data_dict['Category']=response.url.split("/")[3]
+            data_dict['Subcategories']=''
+            data_dict['Product Page URL']=response.url
+            data_dict["Attachement"]= response.xpath("//a[contains(text(),'DOWNLOAD')]//@href").extract()
+            data_dict['Image URL']= images
+            
+            yield data_dict
+    def parse_variations(self,response):
+        print("***********variation********----->",response.url)
+        data_dict= {}
+        desc =response.css("div#prod-overview ::text").extract()
+        desc= ''.join(desc)
+        desc2 = response.css("div.short-description ::text").extract()
+        desc2 = ''.join(desc2)
+        desc = desc.strip() + desc2.strip()
+        data_dict['Seller Platform']= 'Accutron'
+        data_dict['Seller SKU']= response.css('sku.prod-details__sku ::text').extract_first()
+        data_dict['Manufacture']='Accutron'
+        data_dict['Manufacture Code']=response.css('sku.prod-details__sku ::text').extract_first()
+        data_dict['Product Title']= response.css("span.page__title ::text").extract_first()
+        data_dict['Description']=desc
+        data_dict['Packaging']=''
+        data_dict['Qty']= ''
+        data_dict['Category']=response.url.split("/")[3]
+        data_dict['Subcategories']=''
+        data_dict['Product Page URL']=response.url
+        data_dict["Attachement"]= response.xpath("//a[contains(text(),'DOWNLOAD')]//@href").extract()
+        data_dict['Image URL']= response.css("li.easyzoom,easyzoom--overlay>a>img").css('::attr(src)').extract()
+        yield data_dict
+        
+   
+        
+
+    
+
+
+process = CrawlerProcess()
+process.crawl(Accutron_scraper)
+process.start()
---
+++ acradiocom.py
@@ -0,0 +1,93 @@
+import scrapy 
+from scrapy.crawler import CrawlerProcess
+
+
+class acradiocom_scraper(scrapy.Spider):
+    
+    custom_settings = {
+        'DOWNLOAD_DELAY' : 0.25,
+        'RETRY_TIMES': 10,
+        # export as CSV format
+        'FEED_FORMAT' : 'csv',
+        'FEED_URI' : 'acradiocom.com-sample-data.csv'
+    #     "ROTATING_PROXY_LIST" : ["108.59.14.208:13040", "108.59.14.203:13040"],
+    #             "DOWNLOADER_MIDDLEWARES" : {
+    #             "rotating_proxies.middlewares.RotatingProxyMiddleware" : 610,
+    #             "rotating_proxies.middlewares.BanDetectionMiddleware" : 620}
+    }
+     
+    name= 'scraper'
+    start_urls= ['https://acradiocom.com/products/']
+    
+    def parse(self,response):
+        links = response.css("li.ty-subcategories__item>a::attr(href)").extract()
+        
+
+        for link in links:
+
+            yield scrapy.Request(url=link, callback=self.parse_p_links)
+    def parse_p_links(self,response):
+        
+        links  =response.css("div.cm-gallery-item>a ::attr(href)").extract()
+        if len(links)<1:
+            links =response.css("div.ty-grid-list__image>a::attr(href)").extract()
+        
+
+        
+        for link in links:
+            yield scrapy.Request(url=link, callback=self.parse_products)
+            
+            
+        next_page= response.css("a.ty-pagination__item::attr(href)").extract()
+        
+        if next_page is not None:
+            for next in next_page:
+                yield response.follow(url=next, callback=self.parse_p_links)
+    def parse_products(self,response):
+        
+        desc =response.css("div#content_description>div>p::text").extract()
+        desc= ''.join(desc)
+        desc1= response.css("dl.featureBox>dd::text").extract()
+        desc1= ''.join(desc1)
+        desc1= desc1.strip().replace("\r",'')
+        desc1= desc1.strip().replace("\t",'')
+        desc1= desc1.strip().replace("\n",'')
+        desc2 =response.xpath("//div[@class='accordion-content']/ul//text()").extract()
+        desc2 = ''.join(desc2)
+        desc3 = response.css("div#content_description>div ::text").extract()
+        desc3 = ''.join(desc3)
+        desc= desc+ desc1 +desc2 +desc3
+        sku = response.css("span.ty-control-group__item::text").extract_first()
+        if "In stock" or "On backorder" in sku:
+            sku= response.css("h1.ty-product-block-title>bdi::text").extract_first()
+        
+        data_dict={}
+        
+        data_dict['Seller Platform']= 'AC Radio'
+        data_dict['Seller SKU']= sku
+        data_dict['Manufacture']= 'AC Radio'
+        data_dict['Manufacture Code']= sku
+        data_dict['Product Title']= response.css("h1.ty-product-block-title>bdi::text").extract_first()
+        data_dict['Description']= desc
+        data_dict['Packaging']=''
+        data_dict['Qty']=''
+        data_dict['Category']= response.xpath("(//a[@class='ty-breadcrumbs__a']/text())[3]").extract_first()
+        data_dict['Subcategories']=''
+        data_dict['Product Page URL']= response.url
+        data_dict['Attachements']= response.xpath("//p[contains(text(),'For detailed warranty ')]/a/@href").extract()
+        data_dict['Image URL']=response.css("img.ty-pict::attr(src)").extract()
+        yield data_dict
+        
+        
+        
+   
+        
+      
+ 
+    
+    
+
+
+process = CrawlerProcess()
+process.crawl(acradiocom_scraper)
+process.start()
